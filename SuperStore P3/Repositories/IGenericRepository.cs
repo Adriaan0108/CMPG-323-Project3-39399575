50516112cd95ce73b842370c2e69d432a89fa0ae
Adriaan0108 <japienaar7@gmail.com>
2023-09-20 19:43:04 +0200

    Added Generic Repos

---
+++ SuperStore P3/Repositories/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace EcoPower_Logistics.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Remove(T entity);
+        // implement Update() method
+        void Update(T entity);
+    }
+}
