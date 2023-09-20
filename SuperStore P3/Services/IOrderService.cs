923cc4c1157bac5de8adbe3e2706f4ed2d9f12b6
Adriaan0108 <japienaar7@gmail.com>
2023-09-20 21:03:50 +0200

    Added Order Services

---
+++ SuperStore P3/Services/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+
+namespace EcoPower_Logistics.Services
+{
+    public interface IOrderService
+    {
+        Order GetOrderById(int? id);
+        IEnumerable<Order> GetAllOrders();
+        void AddOrder(Order order);
+        void RemoveOrder(Order order);
+        void UpdateOrder(Order order);
+    }
+}
