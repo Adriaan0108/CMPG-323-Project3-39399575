59188c306d8f056178473f4f80d9212c704fe82d
Adriaan0108 <japienaar7@gmail.com>
2023-09-20 22:53:08 +0200

    Added Customer Repos

---
+++ SuperStore P3/Repositories/CustomerRepository.cs
@@ -0,0 +1,31 @@
+﻿using Data;
+using Models;
+
+namespace EcoPower_Logistics.Repositories
+{
+    public class CustomerRepository : GenericRepository<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(SuperStoreContext context) : base(context) { }
+
+        public Customer GetCustomerById(int? id)
+        {
+            return GetAll().FirstOrDefault(m => m.CustomerId == id);
+        }
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            return GetAll().ToList();
+        }
+        public void AddCustomer(Customer customer)
+        {
+            Add(customer);
+        }
+        public void UpdateCustomer(Customer customer)
+        {
+            Update(customer);
+        }
+        public void RemoveCustomer(Customer customer)
+        {
+            Remove(customer);
+        }
+    }
+}
