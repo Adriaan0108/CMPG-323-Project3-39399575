0bfbb124ef0c4988a9b9d6c8437e33b6ce539bad
Adriaan0108 <japienaar7@gmail.com>
2023-09-20 22:32:34 +0200

    Added Product Repos

---
+++ SuperStore P3/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+
+namespace EcoPower_Logistics.Repositories
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+        Product GetProductById(int? id);
+        IEnumerable<Product> GetAllProducts();
+        void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void RemoveProduct(Product product);
+    }
+}
