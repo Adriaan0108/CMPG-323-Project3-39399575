a07ba022c3487049bd68e7525a2bbd95b83ab6b8
Adriaan0108 <japienaar7@gmail.com>
2023-09-21 09:54:46 +0200

    Added Product Service

---
+++ SuperStore P3/Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+
+namespace EcoPower_Logistics.Services
+{
+    public interface IProductService
+    {
+        Product GetProductById(int? id);
+        IEnumerable<Product> GetAllProducts();
+        void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void RemoveProduct(Product product);
+    }
+}
---
+++ SuperStore P3/Services/ProductService.cs
@@ -0,0 +1,35 @@
+﻿using EcoPower_Logistics.Repositories;
+using Models;
+
+namespace EcoPower_Logistics.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _prodRepo;
+        public ProductService(IProductRepository prodRepo) 
+        { 
+            _prodRepo = prodRepo;
+        }
+
+        public Product GetProductById(int? id)
+        {
+            return _prodRepo.GetAll().FirstOrDefault(m => m.ProductId == id);
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _prodRepo.GetAll().ToList();
+        }
+        public void AddProduct(Product product)
+        {
+            _prodRepo.Add(product);
+        }
+        public void UpdateProduct(Product product)
+        {
+            _prodRepo.Update(product);
+        }
+        public void RemoveProduct(Product product)
+        {
+            _prodRepo.Remove(product);
+        }
+    }
+}
