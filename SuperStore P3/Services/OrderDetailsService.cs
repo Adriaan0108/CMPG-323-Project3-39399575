1d5ea22fedd8812e2ac95a70e96ce4f4efd955b9
Adriaan0108 <japienaar7@gmail.com>
2023-09-21 10:04:29 +0200

    Added OrderDetails Services

---
+++ SuperStore P3/Services/OrderDetailsService.cs
@@ -0,0 +1,33 @@
+﻿using Models;
+
+namespace EcoPower_Logistics.Services
+{
+    public class OrderDetailsService : IOrderDetailsService
+    {
+        private readonly IOrderDetailsRepository _detailsRepo;
+        public OrderDetailsService(IOrderDetailsRepository detailsRepo) 
+        { 
+            _detailsRepo = detailsRepo;
+        }
+        public OrderDetail GetOrderDetailById(int? id)
+        {
+            return _detailsRepo.GetAll().FirstOrDefault(m => m.OrderDetailsId == id);
+        }
+        public IEnumerable<OrderDetail> GetAllOrderDetail()
+        {
+            return _detailsRepo.GetAll().ToList();
+        }
+        public void AddOrderDetail(OrderDetail orderDetail)
+        {
+            _detailsRepo.Add(orderDetail);  
+        }
+        public void RemoveOrderDetail(OrderDetail orderDetail)
+        {
+            _detailsRepo.Remove(orderDetail);   
+        }
+        public void UpdateOrderDetail(OrderDetail orderDetail)
+        {
+            _detailsRepo.Update(orderDetail);
+        }
+    }
+}
